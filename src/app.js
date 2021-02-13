0b4d467898a7648969cf901a7861f1d29e2d7510
Simone <simone.sichili@gmail.com>
2021-02-13 18:08:12 +0100

    Complete navbar with vue.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 import Vue from 'vue';
+import VueScrollactive from 'vue-scrollactive';
+
+Vue.use(VueScrollactive);
 
 new Vue({
     el: '#app',
     data: {
-        message: "Hello World"
+        navbarShow: false,
+    },
+    methods: {
+        reloadPage: function() {
+            location.reload();
+            document.documentElement.scrollTop = 0;
+        },
+        showNav: function() {
+            
+            if(this.navbarShow == false) {
+                this.navbarShow = true;
+            } else {
+                this.navbarShow = false;
+            }
+        },
     },
     mounted: function() {
     }
 });
+
+// 
