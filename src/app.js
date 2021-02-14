47ee09542a8bd89e35d859b5fd275ccae3ba66cc
Simone <simone.sichili@gmail.com>
2021-02-14 16:42:11 +0100

    Add "about" section. Fix "hero" section

--- src/app.js
+++ src/app.js
@@ -2,28 +2,29 @@ import Vue from 'vue';
 import VueScrollactive from 'vue-scrollactive';
 
 Vue.use(VueScrollactive);
+var dayjs = require('dayjs');
 
 new Vue({
     el: '#app',
     data: {
         navbarShow: false,
+        currentDate: "",
     },
     methods: {
-        reloadPage: function() {
+        reloadPage: function () {
             location.reload();
             document.documentElement.scrollTop = 0;
         },
-        showNav: function() {
-            
-            if(this.navbarShow == false) {
+        showNav: function () {
+
+            if (this.navbarShow == false) {
                 this.navbarShow = true;
             } else {
                 this.navbarShow = false;
             }
         },
     },
-    mounted: function() {
+    mounted: function () {
+        this.currentDate = dayjs().format('YYYY');
     }
 });
-
-// 
